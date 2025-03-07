07ae01e92eb7662e4c830fb53e9c9fde2286cc18
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-03-07 15:27:18 -0600

    Server-side pre-rendering - hello.tsx file has been created

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Hello from "@/app/components/hello";
+
+export default function Home() {
+  console.log ('Is this a server or client?  -- SERVER SIDE');
+  return (
+    <>
+      <div className="text-3xl">Welcome to Next.JS</div>
+      <Hello/>
+    </>
+    
+  )
+}
