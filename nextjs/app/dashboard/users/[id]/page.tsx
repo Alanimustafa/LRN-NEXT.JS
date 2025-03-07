51fadcd5cdf96a489e1a77d7b6f9ef93556bfae8
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-03-07 17:55:46 -0600

    Using the id params to creared dynamic page for each usr

--- nextjs/app/dashboard/users/[id]/page.tsx
+++ nextjs/app/dashboard/users/[id]/page.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link"
-const page = () => {
+import React from "react"
+
+
+// This is the dynamic route for the user detail page. Note the :id parameter in the path. This id came from the folder we had created in the users folder. If we named the folder "name" folder "id", then the :id would be "name".
+const page = ({params}: { params: {id: string}}) => {
+
+    // we need to destructure the id from the params object.
+    const { id } = params;
   return (
     <>
     <div>
-        <h1>USER DETAIL PAGE</h1>
+        <h1>USER {id} DETAIL PAGE </h1>
         <p>This is the user detail page.</p>
     </div>
     <div className="homePageLink">
